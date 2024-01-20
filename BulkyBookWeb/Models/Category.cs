cdb7d0b95c79ec4f811fd0d57d84886e5507206f
AnkitaTarento <ankita.kaushik@tarento.com>
2024-01-20 16:51:15 +1300

    Add project files.

---
+++ BulkyBookWeb/Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BulkyBookWeb.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public required string Name{ get; set; }
+        [DisplayName("Display Order")]
+        [Range(1,100, ErrorMessage="Display Order must be between 1 to 100 only!!")]
+        public int DisplayOrder { get; set; }
+        public DateTime CreateDateTime { get; set; } = DateTime.Now;
+
+    }
+}
